c276b8384019a455c2825d2bd8ce5e8f9b3b487c
Xavier Leprêtre <xavier.lepretre@gmail.com>
2023-10-31 16:07:05 +0400

    Unhide academy weeks.

--- ida-customizations/.vuepress/config.js
+++ ida-customizations/.vuepress/config.js
@@ -154,7 +154,7 @@ module.exports = {
                 },
               ],
             },
-/*            {
+            {
               title: "Week 1 - Introduction to the Interchain",
               directory: true,
               order: 2,
@@ -580,7 +580,7 @@ module.exports = {
               directory: true,
               order: 9,
               path: "/academy/whats-next/",
-            },*/
+            },
           ],
         },
       ],
